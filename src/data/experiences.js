1dc4a222d09046f3592b6834afdd4b0344ecb960
lamula21 <jvaldiv8@montgomerycollege.edu>
2024-01-19 20:48:52 -0500

    todo

---
+++ src/data/experiences.js
@@ -0,0 +1,59 @@
+import { LucideGraduationCap } from "lucide-react"
+import React from "react"
+
+export const experiencesData = [
+	{
+		title: "Lead Intern Full Stack Developer",
+		location: "Washington, D.C",
+		company: "D.C Code Officials Assoc.",
+		description:
+			"I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+		icon: React.createElement(LucideGraduationCap),
+		date: "2019",
+	},
+	{
+		title: "Group Project",
+		location: "College Park, MD",
+		company: "University of Maryland",
+		description:
+			"I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+		icon: React.createElement(),
+		date: "Jan 2023 - May 2023",
+	},
+	{
+		title: "ADS Note Taker",
+		location: "College Park, MD",
+		company: "University of Maryland",
+		description:
+			"I worked as a note taker for the course of CMSC 335 Web Development with Javascript. Collaborated with the professor and students to provide detailed and organized documentation of lectures.",
+		icon: React.createElement(FaReact),
+		date: "Jan 2023 - May 2023",
+	},
+]
+
+// export const experiencesData = [
+// 	{
+// 		title: "Lead Software Developer",
+// 		location: "Miami, FL",
+// 		description:
+// 			"I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+// 		icon: React.createElement(LucideGraduationCap),
+// 		date: "2019",
+// 	},
+// 	{
+// 		title: "Group Project",
+// 		location: "Orlando, FL",
+// 		description:
+// 			"I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+// 		icon: React.createElement(),
+// 		date: "2019 - 2021",
+// 	},
+// 	{
+// 		title: "ADS Note Taker",
+// 		location: "College Park, MD",
+// 		description:
+// 			"I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+// 		icon: React.createElement(FaReact),
+// 		date: "Jan 2023 - May 2023",
+// 	},
+// ]
